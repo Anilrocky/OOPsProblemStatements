497b6ea54b9ec282298b846b59846c24d5a9753c
Anilrocky <anilkumarac96@gmail.com>
2023-03-16 13:05:18 +0530

    [Anil] Add:Inventory data management of Rice,Pulses,Wheat

--- OOPsProblemStatements/InventoryDataManagement/InventoryOperation.cs
+++ OOPsProblemStatements/InventoryDataManagement/InventoryOperation.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-
 namespace OOPsProblemStatements.InventoryDataManagement
 {
     public class InventoryOperation
